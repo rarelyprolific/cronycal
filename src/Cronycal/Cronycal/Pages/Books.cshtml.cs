6a587c934d7896c8ef7eb42ddb20b632bcd0ad08
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2026-02-23 20:07:27 +0000

    Adding example status message.

--- src/Cronycal/Cronycal/Pages/Books.cshtml.cs
+++ src/Cronycal/Cronycal/Pages/Books.cshtml.cs
@@ -5,6 +5,8 @@ namespace Cronycal.Pages
 {
     public class BooksModel : PageModel
     {
+        public string StatusMessage { get; set; } = "Your books page.";
+
         public void OnGet() { }
     }
 }
