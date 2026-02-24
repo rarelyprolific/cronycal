da8cae8c0a9d2c5b322b78dd391d28f5e7f3ba15
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2026-02-24 21:44:11 +0000

    Added OnConfiguring to BooksContext as temporary solution for scripting migrations

--- src/Cronycal/Cronycal/Data/BooksContext.cs
+++ src/Cronycal/Cronycal/Data/BooksContext.cs
@@ -6,4 +6,13 @@ namespace Cronycal.Data;
 public class BooksContext : DbContext
 {
     public DbSet<Book> Books => Set<Book>();
+
+    // FIXME: Added so I can generate SQL via `dotnet ef migrations script --idempotent --context Cronycal.Data.BooksContext`
+    // Replace OnConfiguring with a IDesignTimeDbContextFactory later.
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        optionsBuilder.UseSqlServer();
+    }
 }
